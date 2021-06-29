8418c4f69fe197981d586b61fc728e306c0cd051
Jericho Chua <jerichochua@gmail.com>
2021-06-29 20:27:28 +0800

    router first test

--- src/App.js
+++ src/App.js
@@ -1,9 +1,37 @@
-function App() {
+import {
+  HashRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+
+export default function App() {
   return (
-    <div className="container">
-      <h1>Home page</h1>
-    </div>
+    <Router>
+      <div className="container">
+        <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/contact">
+            <Contact />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
-export default App;
+function Home() {
+  return <h2>Home</h2>;
+}
+
+function About() {
+  return <h2>About</h2>;
+}
+
+function Contact() {
+  return <h2>Contact</h2>;
+}

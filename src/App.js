976e1fe2907aef64723caca3843b74b578cc3c9e
Jericho Chua <jerichochua@gmail.com>
2022-02-15 18:58:11 +0800

    Remove react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,37 +1,7 @@
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route
-} from "react-router-dom";
-
 export default function App() {
-  return (
-    <Router basename="/react-test">
-      <div className="container">
-        <Switch>
-          <Route path="/about">
-            <About />
-          </Route>
-          <Route path="/contact">
-            <Contact />
-          </Route>
-          <Route path="/">
-            <Home />
-          </Route>
-        </Switch>
-      </div>
-    </Router>
-  );
-}
-
-function Home() {
-  return <h2>Home</h2>;
-}
-
-function About() {
-  return <h2>About</h2>;
-}
-
-function Contact() {
-  return <h2>Contact</h2>;
+    return (
+        <div className="container">
+            <h2>react-test</h2>
+        </div>
+    );
 }
